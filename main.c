14fb6fc6c5647a8e29a9e517f4d9dbaef299a8ba
wearrrrr <contact@wearr.dev>
2025-06-06 23:49:33 -0500

    image is much more stable now

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
 
 int main(int argc, char **argv)
 {
-    efi_runtime_services_t *RS = ST->RuntimeServices;
     ST->ConOut->ClearScreen(ST->ConOut);
-    printf("Hello World!\n");
+    efi_runtime_services_t *RS = ST->RuntimeServices;
 
     efi_guid_t gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
     efi_gop_t *gop = NULL;
@@ -14,18 +13,60 @@ int main(int argc, char **argv)
     BS->LocateProtocol(&gopGuid, NULL, (void**)&gop);
 
     uint32_t *fb = (uint32_t*)gop->Mode->FrameBufferBase;
+    uint32_t screen_width = gop->Mode->Information->HorizontalResolution;
+    uint32_t screen_height = gop->Mode->Information->VerticalResolution;
     uint32_t stride = gop->Mode->Information->PixelsPerScanLine;
+    efi_input_key_t key;
 
-    for (uint16_t y = 0; y < OTHER_HOUSE_HEIGHT; y++) {
-        for (uint16_t x = 0; x < OTHER_HOUSE_WIDTH; x++) {
-            uint32_t image_index = y * OTHER_HOUSE_WIDTH + x;
-            uint32_t fb_index = y * stride + x;
-            fb[fb_index] = other_house[image_index];
+    int32_t x = 0, y = 0, dx = 4, dy = 3;
+    int32_t prev_x = 0, prev_y = 0;
+
+    while (true) {
+        if (ST->ConIn->ReadKeyStroke(ST->ConIn, &key) == EFI_SUCCESS)
+            break;
+
+        uint32_t erase_w = OKAYU_WIDTH;
+        uint32_t erase_h = OKAYU_HEIGHT;
+
+        if (prev_x + erase_w > screen_width)
+            erase_w = screen_width - prev_x;
+
+        if (prev_y + erase_h > screen_height)
+            erase_h = screen_height - prev_y;
+
+        for (uint32_t row = 0; row < erase_h; row++) {
+            uint32_t fb_y = prev_y + row;
+            if (fb_y >= screen_height) continue;
+
+            uint32_t *row_ptr = fb + fb_y * stride + prev_x;
+            memset(row_ptr, 0, erase_w * sizeof(uint32_t));
         }
+
+        for (uint32_t row = 0; row < OKAYU_HEIGHT; row++) {
+            int fb_y = y + (int)row;
+            if (fb_y < 0 || fb_y >= screen_height)
+                continue;
+
+            for (uint32_t col = 0; col < OKAYU_WIDTH; col++) {
+                int fb_x = x + (int)col;
+                if (fb_x < 0 || fb_x >= screen_width)
+                    continue;
+
+                fb[fb_y * stride + fb_x] = okayu[row * OKAYU_WIDTH + col];
+            }
+        }
+
+        prev_x = x;
+        prev_y = y;
+        x += dx;
+        y += dy;
+
+        if (x <= 0 || x + OKAYU_WIDTH >= screen_width) dx = -dx;
+        if (y <= 0 || y + OKAYU_HEIGHT >= screen_height) dy = -dy;
+
+        ST->BootServices->Stall(16667); // about 24fps
     }
 
-    efi_input_key_t key;
-    while (ST->ConIn->ReadKeyStroke(ST->ConIn, &key) != EFI_SUCCESS);
     ST->RuntimeServices->ResetSystem(EfiResetShutdown, 0, 0, u"");
     return EFI_SUCCESS;
 }
